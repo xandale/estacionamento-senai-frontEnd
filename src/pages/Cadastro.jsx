9e87768d199a1d4d41cd3c5436b51d7f51d63214
dustay29 <dustaydustay@gmail.com>
2025-06-18 19:38:43 -0300

    Authentication

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -4,14 +4,18 @@ import { useNavigate } from "react-router-dom";
 
 function Cadastro() {
     const [nome, setNome] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [telefone, setTelefone] = useState('')
     const [email, setEmail] = useState('')
+    const [tipo_usuario, setTipo_usuario] = useState('Aluno'); 
     const [senha, setSenha] = useState('')
+
     const navigate = useNavigate();
 
 
     async function handleSubmit (event) {
         event.preventDefault();
-        console.log(nome, email, senha);
+        console.log(nome, cpf, telefone, email, tipo_usuario, senha);
         try {
             const resposta = await fetch("https://api-tarefas-20dr.onrender.com/cadastro", {
               method:"Post",
@@ -20,7 +24,10 @@ function Cadastro() {
               },
               body: JSON.stringify({
                 nome,
+                cpf,
+                telefone,
                 email,
+                tipo_usuario,
                 senha
               })
             });
@@ -48,11 +55,30 @@ function Cadastro() {
                     onChange={(event) => setNome(event.target.value)}
                 />
                 <input
+                    type="text"
+                    placeholder="Digite seu CPF"
+                    value={cpf}
+                    onChange={(event) => setCpf(event.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Digite seu telefone"
+                    value={telefone}
+                    onChange={(event) => setTelefone(event.target.value)}
+                />
+                <input
                     type="email"
                     placeholder="Digite seu email"
                     value={email}
                     onChange={(event) => setEmail(event.target.value)}
                 />
+                <select
+                    value={tipo_usuario}
+                    onChange={(event) => setTipo_usuario(event.target.value)}
+                    />
+                    <option value="Aluno">Aluno</option>
+                    <option value="Professor">Professor</option>
+                    <option value="Admin">Admin</option>
                 <input
                     type="password"
                     placeholder="Digite sua senha"
